e8c03f3afeb5bc81e51030b4a842d36beb7e33dc
Ram Kaniyur <quadrupleslap@gmail.com>
2019-03-03 14:19:23 +1100

    Fix the cache, hopefully.

--- server/src/cached.js
+++ server/src/cached.js
@@ -8,17 +8,36 @@ module.exports = class Cached {
         };
         this.expiry = 0;
         this.cached = null;
+        this.queue = [];
     }
-    async serve(req, res) {
-        if (Date.now() > this.expiry) {
-            try {
-                this.cached = JSON.stringify(await this.load());
-            } catch {
-                return res.sendStatus(500);
-            }
-            this.expiry = Date.now() + this.maxage;
+
+    serve(req, res) {
+        if (this.queue.length) {
+            this.queue.push(res);
+        } else if (Date.now() > this.expiry) {
+            this.queue.push(res);
+            this.refresh();
+        } else {
+            res.set(this.headers)
+            res.send(this.cached);
         }
-        res.set(this.headers)
-        return res.send(this.cached);
+    }
+
+    async refresh() {
+        try {
+            this.cached = JSON.stringify(await this.load());
+            this.expiry = Date.now() + this.maxage;
+
+            for (let res of this.queue) {
+                res.set(this.headers)
+                res.send(this.cached);
+            }
+        } catch {
+            for (let res of this.queue) {
+                res.sendStatus(500);
+            }
+        }
+
+        this.queue = [];
     }
 };
