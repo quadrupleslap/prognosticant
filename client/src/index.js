16e46e5427177f712980e565ff541e13c5743e7c
Ram Kaniyur <quadrupleslap@gmail.com>
2019-03-10 12:24:54 +1100

    Implement the router and add some placeholder styles.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { NavLink, Router } from './router';
 
-const HomestuckReference = props => {
-    return <marquee>{props.message}</marquee>;
+//TODO: Everything.
+
+const ROUTES = {
+    '/': () => <p><marquee>This is the beginning of something really excellent.</marquee></p>,
+    '/timetable': () => <p>The timetable.</p>,
+    '/buses': () => <p>The buses.</p>,
+    '/settings': () => <p>The settings.</p>,
 };
 
-ReactDOM.render(<HomestuckReference message='This is the beginning of something really excellent.' />, document.getElementById('root'));
+const FALLBACK = () => <p>Page not found.</p>;
+
+ReactDOM.render(
+    <div>
+        <NavLink activeClassName='active' href='/'>Home</NavLink>
+        {' '}
+        <NavLink activeClassName='active' href='/timetable'>Timetable</NavLink>
+        {' '}
+        <NavLink activeClassName='active' href='/buses'>Buses</NavLink>
+        {' '}
+        <NavLink activeClassName='active' href='/settings'>Settings</NavLink>
+
+        <Router routes={ROUTES} fallback={FALLBACK} />
+    </div>,
+    document.getElementById('root'),
+);
---
+++ client/src/router.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { escapeRegExp } from './util.js';
+
+export class Router extends React.Component {
+    constructor(props) {
+        super(props);
+        this._routeChange = this._routeChange.bind(this);
+        this.state = { active: this._resolve() };
+    }
+
+    componentDidMount() {
+        window.addEventListener('route-change', this._routeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('route-change', this._routeChange);
+    }
+
+    render() {
+        return this.state.active;
+    }
+
+    _routeChange() {
+        this.setState({ active: this._resolve() });
+    }
+
+    _resolve() {
+        let path = decodeURIComponent(location.pathname);
+
+        for (let key in this.props.routes) {
+            let match = new RegExp('^' + key + '$').exec(path);
+            if (match) return this.props.routes[key](...match);
+        }
+
+        return this.props.fallback(path);
+    }
+}
+
+export function Link(props) {
+    let { onClick: oldOnClick, ...rest } = props;
+
+    let onClick = e => {
+        e.preventDefault();
+        navigate(props.href);
+        if (oldOnClick) oldOnClick(e);
+    };
+
+    return <a {...rest} onClick={onClick} />;
+}
+
+export function NavLink(props) {
+    let { activeClassName, ...rest } = props;
+
+    let on = () => <Link {...rest} className={activeClassName} />;
+    let off = () => <Link {...rest} />;
+
+    return <Router routes={{ [escapeRegExp(props.href)]: on }}
+                   fallback={off} />;
+}
+
+export function navigate(href) {
+    history.pushState(null, null, href);
+    window.dispatchEvent(new Event('route-change'));
+}
+
+window.addEventListener('popstate', () => {
+    window.dispatchEvent(new Event('route-change'));
+});
